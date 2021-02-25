d63d278e4c89647697e87b402e8f9bc40923b04b
Kosyuchenko Anton <greenforse1996@gmail.com>
2021-02-25 19:01:19 +0500

    MarkCommand

--- Command.py
+++ Command.py
@@ -49,4 +49,42 @@ def ShowSelectCommand():
 def DeselectStudentCommand():
   Edit_context().student=None
 
+def EditFirstNameCommand():
+  fname=input("Введите новое имя")
+  Edit_context().student.insert(0,fname)
+  del Edit_context().studen[1]
+
+def EditMidleNameCommand():
+  MName=input("Введите новую фамилию")
+   Edit_context().student.insert(1,MName)
+   del Edit_context().studen[2]
   
+def EditLastNameCommand():
+  LName=input("Введите новое отчесво")
+   Edit_context().student.insert(2,LName)
+   del Edit_context().studen[3]
+
+def EditGroupCommand():
+  group=input("Введите новую группу")
+   Edit_context().student.insert(3,group)
+   del Edit_context().studen[4]
+
+def AddMarkCommand():
+  mark=input("Введите предмет: ")
+  if mark not in Edit_context.student.marks:
+    score=int(input("Введите оценку: "))
+    Edit_context.student.marks[mark]=score
+  else print("Ошибка: такой предмет уже есть")
+
+def EditMarkCommand():
+  mark=input("Введите предмет: ")
+  if mark  in Edit_context.student.marks:
+    score=int(input("Введите оценку: "))
+    Edit_context.student.marks[mark]=score
+  else print("Ошибка: такой предмета нет")
+
+def DeleteMarkCommand():
+  mark=input("Введите предмет для удаления: ")
+  if mark  in Edit_context.student.marks:
+    del Edit_context.student.marks[mark]
+  else print("Ошибка: такой предмета нет")
